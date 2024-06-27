2c10b2650d8b3ec80f11baccb0fde0ce8f6e9765
webvocado <bsh8164@gmail.com>
2024-06-27 15:44:47 +0900

    라우트 나눴음

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Company from './pages/company/Company';
+import { Recruit } from './pages/recruit/Recruit';
+import Resume from './pages/resume/Resume';
+import User from './pages/user/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/recruit" element={<Recruit/>}></Route>
+          <Route path="/resume" element={<Resume/>}></Route>
+          <Route path="/user" element={<User/>}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
