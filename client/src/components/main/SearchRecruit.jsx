dca72bc6519d1c28f7641941692433fe017a65b4
udangtang <qghdwnsqja@gmail.com>
2024-06-28 02:09:33 +0900

    검색어 80%함...

--- client/src/components/main/SearchRecruit.jsx
+++ client/src/components/main/SearchRecruit.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
-const searchRecruit = ({ recruitPost }) => {
-    const [recruit, setRecruit] = useState(recruitPost || []);
+const SearchRecruit = ({ recruitList }) => {
+    const [recruit, setRecruit] = useState(recruitList || []);
+
+    useEffect(() => {
+        setRecruit(recruitList || []);
+    }, [recruitList]);
+
     return (
-        (
+        <>
+            {recruit.map((recruit) => (
+                <li key={recruit.recruitNo} className="recruit-box custom-dropdown-item2 w-100">
+                    <a
+                        className="recruit-box"
+                        href={`/recruit/detail_jobs_user?recruitNo=${recruit.recruitNo}`}
+                        style={{ display: 'block', height: '100%', padding: '10px' }}
+                    >
+                        {recruit.recruitTitle}
+                    </a>
+                </li>
+            ))}
+        </>
+    );
+};
 
-            <li key={recruit.recruitNo} className="recruit-box custom-dropdown-item2 w-100">
-                <a
-                    className="recruit-box"
-                    href={`/recruit/detail_jobs_user?recruitNo=${recruit.recruitNo}`}
-                    style={{ display: 'block', height: '100%', padding: '10px' }}
-                >
-                    {recruit.recruitTitle}
-                </a>
-            </li>
-
-        )
-
-    )
-}
-
-export default searchRecruit
+export default SearchRecruit;
--- client/src/containers/main/HomeContainer.jsx
+++ client/src/containers/main/HomeContainer.jsx
@@ -3,31 +3,10 @@ import HomeHeader from '../../components/main/HomeHeader'
 import CardList from '../../components/main/CardList'
 
 const HomeContainer = () => {
-    const [keyword, setKeyword] = useState('');
-    const [selectedCode, setSelectedCode] = useState('0');
-    const [optionList, setOptionList] = useState([]);
-
-    useEffect(() => {
-        // 옵션 리스트를 가져오는 API 호출
-        fetch('/api/options')
-            .then(response => response.json())
-            .then(data => setOptionList(data));
-    }, []);
-
-    const handleInputChange = (newKeyword, newCode) => {
-        setKeyword(newKeyword);
-        setSelectedCode(newCode);
-    };
-
     return (
         <div className="container main-content">
-            <HomeHeader 
-                handleInputChange={handleInputChange} 
-                keyword={keyword} 
-                selectedCode={selectedCode}
-                optionList={optionList}
-            />
-            <CardList keyword={keyword} selectedCode={selectedCode} />
+            <HomeHeader/>
+            <CardList/>
         </div>
     )
 }
