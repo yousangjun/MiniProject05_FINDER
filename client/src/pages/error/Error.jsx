759f835e7b99c7b452766a73e505accb11c4840e
jjb <jubin030419@gmail.com>
2024-06-27 18:19:27 +0900

    error 페이지 / contentTitle 완성 - jjb

--- client/src/pages/error/Error.jsx
+++ client/src/pages/error/Error.jsx
@@ -9,7 +9,7 @@ const error = () => {
       <div className='container error-page h-auto'>
          <img src="/img/logo.png" alt="Logo"/>
           <h1>404 not found</h1>
-          <p>웹 페이지를 불러올 수 없습니다.</p>
+          <p>페이지를 불러올 수 없습니다.</p>
           <a href="javascript:hisroty.back()">이전 페이지로 돌아가기</a>
       </div>
       </>
