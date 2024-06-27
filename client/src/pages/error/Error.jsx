9f8fe753c4ff89529e8ba7f53d0d7355c987c268
baek <bsh8164@gmail.com>
2024-06-27 23:36:20 +0900

    결제 리액트

--- client/src/pages/error/Error.jsx
+++ client/src/pages/error/Error.jsx
@@ -10,7 +10,7 @@ const error = () => {
          <img src="/img/logo.png" alt="Logo"/>
           <h1>404 not found</h1>
           <p>페이지를 불러올 수 없습니다.</p>
-          <a href="javascript:hisroty.back()">이전 페이지로 돌아가기</a>
+          <a href="javascript:history.back()">이전 페이지로 돌아가기</a>
       </div>
       </>
     )
