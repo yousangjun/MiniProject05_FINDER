36d444b817c494f19573381dbcd146c62fc19927
jjb <jubin030419@gmail.com>
2024-06-28 13:18:18 +0900

    login 이제 막 진행하려고 노력 중

---
+++ client/src/containers/user/LoginContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../../components/user/Login'
+
+const LoginContainer = () => {
+  return (
+    <div  className='container main-content'>
+        <Login/>
+    </div>
+  )
+}
+
+export default LoginContainer
--- client/src/pages/resume/Introduce_com.jsx
+++ client/src/pages/resume/Introduce_com.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Header from '../../layouts/Header'
 import Footer from '../../layouts/Footer'
-import BtnShort from '../../components/main/BtnShort'
-import ContentTitle from '../../components/resume/ContentTitle'
 import Introduce_com_Container from '../../containers/resume/Introduce_com_Container'
 
 const Introduce_com = () => {
   return (
     <>
     <Header/>
-    <ContentTitle SubTitle1={"기업 소개"} SubTitle2={"사람을 소중히 대하라"}/>
-    <BtnShort btnShortText={"수정"}/>
     <Introduce_com_Container/>
     <Footer/>
     </>
