858bd43cfda041c997d0cac97f429a2c9cc06f9c
udangtang <qghdwnsqja@gmail.com>
2024-06-29 23:26:59 +0900

    newjobs

--- client/src/containers/recruit/NewJobsUserContainer.jsx
+++ client/src/containers/recruit/NewJobsUserContainer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Sidebar from '../../components/main/Sidebar'
 import ContentListItem from '../../components/main/ContentListItem'
-import ContentTitle from '../../components/resume/ContentTitle'
 import ContentHeader from '../../components/main/ContentHeader'
+import Sidebar from '../../components/main/Sidebar'
+import ContentTitle from '../../components/resume/ContentTitle'
 
-const AppliedJobsUserContainer = () => {
-
+const NewJobsUserContainer = () => {
     return (
         <>
             <ContentTitle SubTitle1={"채용공고 관리"} SubTitle2={"열정과 재능이 만나는 곳, 당신의 꿈이 시작됩니다."} />
@@ -13,8 +12,8 @@ const AppliedJobsUserContainer = () => {
                 <Sidebar />
                 <main1 className="col-12 col-md-12 col-lg-8 main-content1 d-flex justify-content-end align-items-start">
                     <div className="job-listings1">
-                        <ContentHeader ContentHeaderText={"지원 한 채용공고"}/>
-                        <ContentListItem ContentListItemText={"합격조회"} ShowBtn={true} />
+                        <ContentHeader ContentHeaderText={"최근 본 채용공고"} />
+                        <ContentListItem />
                     </div>
                 </main1>
             </div>
@@ -22,4 +21,4 @@ const AppliedJobsUserContainer = () => {
     )
 }
 
-export default AppliedJobsUserContainer
+export default NewJobsUserContainer
