7ce2191082da84cd7a87bdf5bd5722df88f8b024
baek <bsh8164@gmail.com>
2024-06-30 10:23:02 +0900

    라우터정리

--- client/src/App.js
+++ client/src/App.js
@@ -21,14 +21,13 @@ import Login from './pages/user/Login';
 import CvCreate_user from './pages/resume/CvCreate_user';
 
 
-import Join_user from './pages/user/join_user';
-import Join_com from './pages/user/join_com';
 import Post_jobs_com from './pages/resume/Post_jobs_com';
 import CvRead_user from './pages/resume/CvRead_user';
 
 
 import Applied_jobs_user from "./pages/recruit/Applied_jobs_user";
 import Cv_list_user from "./pages/recruit/Cv_list_user";
+import Detail_jobs_user from "./pages/recruit/detail_jobs_user"
 import New_jobs_user from "./pages/recruit/New_jobs_user";
 import Posted_jobs_com from "./pages/recruit/Posted_jobs_com";
 import Recruit_list_com from "./pages/recruit/Recruit_list_com";
@@ -48,7 +47,7 @@ function App() {
           <Route path="/company/credit_com" element={<Credit_com/>}></Route>
           <Route path="/company/credit_detail_com" element={<Credit_detail_com/>}></Route>
           <Route path="/company/checkout" element={<Checkout/>}></Route>
-          <Route path="/company/Introduce_Com" element={<Introduce_com/>}></Route>
+          <Route path="/company/Introduce_Com" element={<Introduce_com/>}></Route>          {/* 기업소개 등록/수정 */}
           <Route path="/company/success" element={<Success/>}></Route>
           <Route path="/company/fail" element={<Fail/>}></Route>
           <Route path="/company/credit_list_com" element={<Credit_list_com/>}></Route>
@@ -56,21 +55,21 @@ function App() {
           
           {/* 채용공고 페이지 */}
           <Route path="/recruit" element={<Home/>}></Route>
+          <Route path="/recruit/detail_jobs_user" element={<Detail_jobs_user/>}></Route>  {/* 채용공고 상세 [공통] */}
 
-          <Route path="/recruit/detail_jobs_user" element={<Detail_jobs_user/>}></Route>
-          <Route path="/recruit/post_jobs_com" element={<Post_jobs_com/>}></Route>
+          <Route path="/recruit/post_jobs_com" element={<Post_jobs_com/>}></Route>        {/* 채용공고 등록 [기업] */}
+          <Route path="/recruit/recruit_list_com" element={<Recruit_list_com/>}></Route>    {/* 등록한 채용공고 [기업] */}
+          <Route path="/recruit/posted_jobs_com" element={<Posted_jobs_com/>}></Route>    {/* 제출된 이력서 [기업] */}
 
-
-          <Route path="/recruit/applied_jobs_user" element={<Applied_jobs_user/>}></Route>
-          <Route path="/recruit/new_jobs_user" element={<New_jobs_user/>}></Route>
-          <Route path="/recruit/cv_list_user" element={<Cv_list_user/>}></Route>
-          <Route path="/recruit/recruit_list_com" element={<Recruit_list_com/>}></Route>
+          <Route path="/recruit/applied_jobs_user" element={<Applied_jobs_user/>}></Route>  {/* 지원한 채용공고 [구직자] */}
+          <Route path="/recruit/new_jobs_user" element={<New_jobs_user/>}></Route>          {/* 최근본 채용공고 [구직자] */}
+          <Route path="/recruit/cv_list_user" element={<Cv_list_user/>}></Route>            {/* 나의 이력서 [구직자] */}
 
 
           {/* 이력서 페이지 */}
           <Route path="/resume" element={<Resume/>}></Route>
-          <Route path="/resume/cvCreate_user" element={<CvCreate_user/>}></Route>
-          <Route path="/resume/cvRead_user" element={<CvRead_user/>}></Route>
+          <Route path="/resume/cvCreate_user" element={<CvCreate_user/>}></Route>           {/* 이력서 등록 */}
+          <Route path="/resume/cvRead_user" element={<CvRead_user/>}></Route>               {/* 이력서 조회 / 수정 / 삭제 */}
 
 
           {/* 회원 페이지 */}
