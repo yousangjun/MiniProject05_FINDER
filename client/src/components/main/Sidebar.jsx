c5287fab36f7ffdbe6be1dc59966900b7506da67
webvocado <bsh8164@gmail.com>
2024-07-01 21:31:03 +0900

    bsh 자잘구리

--- client/src/components/main/Sidebar.jsx
+++ client/src/components/main/Sidebar.jsx
@@ -1,109 +1,187 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { LoginContext } from '../../contexts/LoginContextProvider'
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+
+  const { roles } = useContext(LoginContext);
+
   return (
-      <nav className="col-12 col-sm-12 col-lg-3 sidebar1" style={{ boxShadow: '-4px 8px 20px rgba(0, 0, 0, 0.1)' }}>
-        <div style={{ margin: '15px 0 45px 15px' }}>
-          <strong style={{ fontSize: '24px' }}>마이페이지</strong>
-        </div>
-        <ul className="nav flex-column ul-header1">
-          <li className="li-header1">
-            <strong className="fs-5">기업 정보</strong>
-          </li>
-          <li className="nav-item1">
-            <a href="/company/introduce_com" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/home.png" alt="Home" />
+    <nav className="col-12 col-sm-12 col-lg-3 sidebar1" style={{ boxShadow: '-4px 8px 20px rgba(0, 0, 0, 0.1)' }}>
+      <div style={{ margin: '15px 0 45px 15px' }}>
+        <strong style={{ fontSize: '24px' }}>마이페이지</strong>
+      </div>
+
+      {roles.isCompany && (
+        <>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">기업 정보</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/company/introduce_com" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/home.png" alt="Home" />
+                  </div>
+                  <div>기업 소개</div>
                 </div>
-                <div>기업 소개</div>
-              </div>
-            </a>
-          </li>
-          <li className="nav-item1">
-            <a href="/user/update_user" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/Registration.png" alt="Registration" />
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/user/update_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/Registration.png" alt="Registration" />
+                  </div>
+                  <div>담당자 정보 수정</div>
                 </div>
-                <div>담당자 정보 수정</div>
-              </div>
-            </a>
-          </li>
-          <li className="nav-item1">
-            <a href="/company/credit_list_com" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/CreditCard.png" alt="Credit Card" />
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/company/credit_list_com" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/CreditCard.png" alt="Credit Card" />
+                  </div>
+                  <div>결제 내역</div>
                 </div>
-                <div>결제 내역</div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <ul className="nav flex-column ul-header1">
-          <li className="li-header1">
-            <strong className="fs-5">채용공고 관리</strong>
-          </li>
-          <li className="nav-item1">
-            <a href="/recruit/post_jobs_com" className="job-item-link1 nav-link1 fw-normal fs-6 token-end">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex justify-content-between">
-                <div className="d-flex gap-2 p-2 ps-2">
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">채용공고 관리</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/post_jobs_com" className="job-item-link1 nav-link1 fw-normal fs-6 token-end">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex justify-content-between">
+                  <div className="d-flex gap-2 p-2 ps-2">
+                    <div>
+                      <img src="/img/list.png" alt="List" />
+                    </div>
+                    <div>채용공고 등록</div>
+                  </div>
+                  <div className="d-flex gap-2 p-2">
+                    <div>
+                      <img src="/img/CreditScore.png" alt="Credit Score" style={{ width: '24px' }} />
+                    </div>
+                    <div>
+                      {/* Replace server-side rendering logic with client-side logic if necessary */}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/recruit_list_com" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
                   <div>
                     <img src="/img/list.png" alt="List" />
                   </div>
-                  <div>채용공고 등록</div>
+                  <div>등록한 채용공고</div>
                 </div>
-                <div className="d-flex gap-2 p-2">
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">이력서 관리</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/posted_jobs_com" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
                   <div>
-                    <img src="/img/CreditScore.png" alt="Credit Score" style={{ width: '24px' }} />
+                    <img src="/img/Documents.png" alt="Documents" />
                   </div>
+                  <div>제출된 이력서</div>
+                </div>
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/company/score_com" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
                   <div>
-                    {/* Replace server-side rendering logic with client-side logic if necessary */}
+                    <img src="/img/Documents.png" alt="Documents" />
                   </div>
+                  <div>AI 간편 평가</div>
                 </div>
-              </div>
-            </a>
-          </li>
-          <li className="nav-item1">
-            <a href="/recruit/posted_jobs_com" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/list.png" alt="List" />
+              </Link>
+            </li>
+          </ul>
+        </>
+      )}
+      {!roles.isCompany && roles.isUser && (
+        <>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">내 정보</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/user/update_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/Registration.png" alt="Registration" />
+                  </div>
+                  <div>내 정보 수정</div>
                 </div>
-                <div>등록한 채용공고</div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <ul className="nav flex-column ul-header1">
-          <li className="li-header1">
-            <strong className="fs-5">이력서 관리</strong>
-          </li>
-          <li className="nav-item1">
-            <a href="/recruit/recruit_list_com" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/Documents.png" alt="Documents" />
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">채용공고 관리</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/applied_jobs_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/list.png" alt="List" />
+                  </div>
+                  <div>지원한 채용공고</div>
                 </div>
-                <div>제출된 이력서</div>
-              </div>
-            </a>
-          </li>
-          <li className="nav-item1">
-            <a href="/company/score_com" className="job-item-link1 nav-link1 fw-normal fs-6">
-              <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
-                <div>
-                  <img src="/img/Documents.png" alt="Documents" />
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/new_jobs_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/list.png" alt="List" />
+                  </div>
+                  <div>최근 본 채용공고</div>
                 </div>
-                <div>AI 간편 평가</div>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    );
-}
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav flex-column ul-header1">
+            <li className="li-header1">
+              <strong className="fs-5">이력서 관리</strong>
+            </li>
+            <li className="nav-item1">
+              <Link to="/recruit/cv_list_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/Documents.png" alt="Documents" />
+                  </div>
+                  <div>나의 이력서</div>
+                </div>
+              </Link>
+            </li>
+            <li className="nav-item1">
+              <Link to="/resume/cvCreate_user" className="job-item-link1 nav-link1 fw-normal fs-6">
+                <div style={{ backgroundColor: '#fff', borderRadius: '8px' }} className="d-flex gap-2 p-2 ps-2">
+                  <div>
+                    <img src="/img/Documents.png" alt="Documents" />
+                  </div>
+                  <div>이력서 작성</div>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </>
+      )}
+    </nav>
+  );
+};
 
-
-export default Sidebar
+export default Sidebar;
--- client/src/containers/resume/Post_jobsReadContainer_com.jsx
+++ client/src/containers/resume/Post_jobsReadContainer_com.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Post_jobs_com from '../../components/resume/Post_jobs_com'
-import BtnLong from '../../components/main/BtnLong'
 import './css/Post_job_btn.css'
 import ContentTitle from '../../components/resume/ContentTitle'
+import Sidebar from '../../components/main/Sidebar'
+import Post_jobsRead_com from '../../components/resume/Post_jobsRead_com'
 
 const Post_jobsReadContainer_com = () => {
   return (
@@ -10,18 +10,13 @@ const Post_jobsReadContainer_com = () => {
       <ContentTitle SubTitle1={"채용공고 관리"}
       SubTitle2={"혁신과 열정을 바탕으로, 미래를 창조하라"}/>
     <div className="row justify-content-center" style={{padding: '0 40px'}}>
-    <main className='col-12 col-md-12 col-lg-8 main-content1 d-flex justify-content-end align-items-start'
-    style={{marginLeft: '50px'}}>
-      <div className='job-listings1'>
-          <Post_jobs_com/>
-          <div className='UpdateBtnR'>
-              <BtnLong btnLongText={"수정"}/>
-          </div>
-          <div className='DeleteBtnR'>
-              <BtnLong btnLongText={"삭제"}/>
-          </div>
-      </div>
-    </main>
+      <Sidebar/>
+      <main className='col-12 col-md-12 col-lg-8 main-content1 d-flex justify-content-end align-items-start'
+      style={{marginLeft: '50px'}}>
+        <div className='job-listings1'>
+            <Post_jobsRead_com/>
+        </div>
+      </main>
     </div>
     </>
   )
